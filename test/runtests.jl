ecf494f2fa912d3f9e87151f0a14e94a681c965a
Robert Moss <rmoss92@gmail.com>
2021-10-23 01:22:55 -0700

    Added tests for full 100% coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,11 @@ using GaussianDiscriminantAnalysis
 
 @test begin
     𝒟 = generate_example_data(100, seed=0)
+    x1_positive = extract_data(𝒟, 1; dims=1)
+    x_positive = extract_data(𝒟, 1)
+    x_negative = extract_data(𝒟, 0)
+    predict, mv_negative, mv_positive = qda(𝒟)
+    predict, mv_negative, mv_positive = lda(𝒟)
     plot_gda(𝒟)
     plot_gda(𝒟, soft=false)
     plot_gda(𝒟, use_qda=false)
